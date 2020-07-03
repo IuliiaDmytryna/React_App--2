501f03fbaa936c1e6ce332f99a0567d97ffca864
Iuliia <iuliiadmytryna@gmail.com>
2020-07-03 17:05:56 +0300

    first component (character details) has been implemented

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,29 @@
 import React from 'react';
-// import {Col, Row, Container} from 'reactstrap';
+import {Col, Row, Container} from 'reactstrap';
 // import Header from '../header';
 // import RandomChar from '../randomChar';
-// import CharDetails from '../charDetails';
+import CharDetails from '../charDetails';
 // import ItemList from '../itemList'
 
 
 const App = () => {
     return (
         <> 
-
+            <Container>
+            </Container>
+            <Container>
+                <Row>
+                    <Col lg={{size: 5, offset: 0}}>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md='6'>
+                    </Col>
+                    <Col md='6'>
+                        <CharDetails/>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 };
